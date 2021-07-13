14b9e39fadb6eaa1c510aeeb6f3a7b4096cab07b
Kateryna Karpova <katyakarpova@gmail.com>
2021-07-13 23:21:28 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,12 @@
-//import '../css/common.css';
-
 const refs = {
     btnStart: document.querySelector('[data-start]'),
     btnStop: document.querySelector('[data-stop]'),
     body: document.querySelector('body'),
 };
 
-console.log(refs.btnStart);
-console.log(refs.btnStop);
-console.log(refs.body);
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-console.log(getRandomHexColor());
 
 let timerId = null;
 
@@ -25,19 +17,16 @@ function onStartClick () {
   timerId = setInterval(() => {
     toChangeBgColor();
   }, 1000);
-    console.log('Start');
 };
 
 function onStopClick () {
     clearInterval(timerId);
     refs.btnStart.removeAttribute('disabled');
-    console.log('Stop');
 };
 
 function toChangeBgColor() {
-    refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+    refs.body.style.backgroundColor = getRandomHexColor();
     refs.btnStart.setAttribute('disabled', true);
-    console.log('changing colors');
 };
 
 
