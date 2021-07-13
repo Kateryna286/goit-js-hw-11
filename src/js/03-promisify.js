15c810448376b14c07385a62fe7438c86f922d18
Kateryna Karpova <katyakarpova@gmail.com>
2021-07-14 00:37:02 +0200

    Update 03-promisify.js
    Subtask 2

--- src/js/03-promisify.js
+++ src/js/03-promisify.js
@@ -19,21 +19,25 @@ const users = [
   { name: 'Ajax', active: false },
 ];
 
-const toggleUserState = (allUsers, username, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === username ? { ...user, active: !user.active } : user
-  );
-
-  callback(updatedUsers);
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve, reject) => {
+    const updatedUsers = allUsers.map(user => {
+      if (user.name === userName) {
+        resolve({ ...user, active: !user.active });
+      }
+    });
+  });
 };
+
+const logger1 = updatedUsers => console.table(updatedUsers);
 
 // Currently the function works like this
 // toggleUserState(users, 'Mango', console.table);
 // toggleUserState(users, 'Ajax', console.table);
 
 // The function should work like this
-// toggleUserState(users, 'Mango').then(console.table);
-// toggleUserState(users, 'Ajax').then(console.table);
+toggleUserState(users, 'Mango').then(console.table);
+toggleUserState(users, 'Ajax').then(console.table);
 
 // ======================= Subtask 3 =======================
 const randomIntegerFromInterval = (min, max) => {
