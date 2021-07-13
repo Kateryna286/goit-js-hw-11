efaec92669c757f79428ab442b5d505397483390
Kateryna Karpova <katyakarpova@gmail.com>
2021-07-13 22:05:13 +0200

    Update timer

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './css/common.css';
-// import './js/01-color-switcher';
-// import './js/02-timer';
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,4 @@
-//import '../css/common.css';
+import Swal from 'sweetalert2'
 
 
 const refs = {
@@ -15,26 +15,53 @@ class Timer {
         this.intervalId = null,
         this.isActive = false,
         this.onTick = onTick;
+
+        this.init();
     }
+
+    init() {
+    const time = this.convertMs(0);
+    this.onTick(time);
+  }
 
     start() {
         if (this.isActive) {
             return;
         }
-
+            
         this.isActive = true;
 
         this.intervalId = setInterval(() => {
             const dateEntered = new Date(refs.input.value);
-            const targetDate = dateEntered.getTime()
+            const targetDate = dateEntered.getTime();
             const currentDate = new Date();
     
             const ms = targetDate - currentDate;
+            
             const time = this.convertMs(ms);
-            console.log(time);
+
+            if (ms <= 0) {
+                Swal.fire({
+                    text: 'Please choose a date in the future',
+                    confirmButtonText: 'Ok'
+                });
+                timer.stop();
+                return;
+            }
+
+            else if (ms < 1000) {
+                timer.stop();
+            }
             
             this.onTick(time);    
         }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        const time = this.convertMs(0);
+        this.onTick(time);
     }
 
     pad(value) {
@@ -74,56 +101,3 @@ function updateTextContent({ days, hours, minutes, seconds }) {
 }
 
 refs.btnStart.addEventListener('click', timer.start.bind(timer));
-
-
-
-
-// const timer = {
-//     intervalId: null,
-//     isActive: false,
-//     start() {
-//         if (this.isActive) {
-//             return;
-//         }
-
-//         this.isActive = true;
-
-//         this.intervalId = setInterval(() => {
-//             const dateEntered = new Date(refs.input.value);
-//             const targetDate = dateEntered.getTime()
-//             const currentDate = new Date();
-    
-//             const ms = targetDate - currentDate;
-//             const time = convertMs(ms);
-//             console.log(time);
-            
-//             updateTextContent(time);
-            
-//         }, 1000);
-//     }
-// };
-
-
-
-// function pad(value) {
-//     return String(value).padStart(2, '0');
-// };
-  
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
